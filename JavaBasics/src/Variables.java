df40cbd78779cff87d5cf9c1778e66cf34bf3140
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2023-06-09 17:16:01 +0300

    Add files via upload

---
+++ JavaBasics/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int integerNum = 10 ;
+        double doubleNum = 3.6 ;
+
+        String name = "Teodor" ;
+        char letter = 'k' ;
+
+        System.out.println(integerNum);
+        System.out.println(doubleNum);
+        System.out.println(name);
+        System.out.println(letter);
+    }
+}
