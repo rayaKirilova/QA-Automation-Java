89859f20a71d20b2975b830e34709fdf868b6ed9
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2023-06-12 21:02:41 +0300

    Add files via upload

---
+++ JavaBasics/src/String_Opr_Ex3_Count_Occurence.java
@@ -0,0 +1,10 @@
+public class String_Opr_Ex3_Count_Occurence {
+    public static void main(String[] args) {
+
+        String sentence = "Maggie is an engineer. Gilbert is an artist." ;
+
+        int length = sentence.length() ; // 44
+
+
+    }
+}
