74facf5019bbcc0dd938be754966a0cb21c389fd
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2023-06-19 07:08:53 +0300

    Add files via upload

---
+++ src/Collections/Collections_Ex3_Copy_Array.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections_Ex3_Copy_Array {
+    public static void main(String[] args) {
+
+        String[] toys = {"bear", "doll", "ball", "truck", "duck", "bunny"};
+        List<String> myNewToysList = new ArrayList<String>();
+
+        int length = toys.length;
+
+        for (int i = 0; i < length; i++) {
+            myNewToysList.add(toys[i]);
+        }
+        System.out.print("My toy list is: ");
+        System.out.println(myNewToysList);
+    }
+}
