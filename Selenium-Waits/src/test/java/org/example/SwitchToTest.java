1ed1ad2c1d3958788d7204c330e48fcf8167e7d4
rayaKirilova <raya.n.kirilova@abv.bg>
2023-07-18 14:49:33 +0300

    Adding files to project

--- Selenium-Waits/src/test/java/org/example/SwitchToTest.java
+++ Selenium-Waits/src/test/java/org/example/SwitchToTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -42,3 +43,49 @@ public class SwitchToTest {
 
     }
 }
+=======
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SwitchToTest {
+    static WebDriver webDriver ;
+
+    @BeforeAll
+    public static void initiate(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions() ;
+        options.addArguments("Start-Maximized") ;
+        // options.addArguments("--remote-allow-origins=*");
+
+        webDriver = new ChromeDriver(options) ;
+    }
+
+    @AfterAll
+    public static void close(){
+        //  webDriver.close();
+        //  webDriver.quit();
+    }
+
+    @Test
+    @DisplayName("Should create the correct alert")
+    public void createAlertCheck(){
+        String expectedResult = "Hello! I am an alert box!" ;
+        SwitchTo switchTo = new SwitchTo(webDriver) ;
+        String realAlertString = switchTo.createAlert() ;
+
+        // test passed
+        Assertions.assertTrue(realAlertString.contains(expectedResult));
+
+        /* test failed - comparison failure with different text
+        String falseResult = "Hello! What's your name?" ;
+        Assertions.assertTrue(realAlertString.contains(falseResult));
+        */
+
+    }
+}
+>>>>>>> origin/main
